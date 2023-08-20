839f2f85caf0ef14ec5a63d9922e7888711f8054
Haalloobim <withbimatara@gmail.com>
2023-08-20 19:30:35 +0700

    feat: add exception for update.php

--- updateConf.php
+++ updateConf.php
@@ -8,7 +8,20 @@ if (isset($_POST['change'])){
     $queryUser = mysqli_query($db, "SELECT * FROM usertest WHERE name ='$uname'"); 
     $queryEmail = mysqli_query($db, "SELECT * FROM usertest WHERE email ='$email'");
 
-    
+    if ($uname !== "" || $email !== ""){
+        header("Location: update.php?status=empty");
+    }
+    else if (mysqli_num_rows($queryUser) == 0){
+        header("Location: update.php?status=usernoexist"); 
+    }
+    else if (mysqli_num_rows($queryEmail) == 0){
+        header("Location: update.php?status=emailnoexist"); 
+    }
+    else {
+
+    }
+
+
 }
 
 
