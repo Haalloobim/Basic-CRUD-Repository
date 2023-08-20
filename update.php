1ab9ea23b3974a0915a58abfbb7afc18786809f4
Haalloobim <withbimatara@gmail.com>
2023-08-20 20:16:13 +0700

    feat: add script for update page and seperate update config

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-include("updateConf.php");
+include("updateUserConf.php");
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +43,7 @@ include("updateConf.php");
                     <p class="text-2xl font-inter font-semibold">Update Username: </p>
                 </div>
                 <div class="container">
-                    <form class="flex flex-col gap-2" action="updateConf.php"  method="POST">
+                    <form class="flex flex-col gap-2" action="updateUSerConf.php"  method="POST">
                     <div class="mb-3  flex flex-col gap-2">
                       <label class="text-lg font-inter font-medium">Old Username :</label>
                       <input class="border-2 border-[#9AA2B1] rounded-md w-64 py-1 px-3 placeholder:text-[#9AA2B1]" placeholder="Username yang ingin diubah..." type="text"  id="Username" name="username"  autocomplete="off" />
@@ -53,10 +53,10 @@ include("updateConf.php");
                       <input class="border-2 border-[#9AA2B1] rounded-md w-64 py-1 px-3 placeholder:text-[#9AA2B1]" placeholder="username baru..." type="text"  id="Username" name="usernameNew"  autocomplete="off" />
                     </div>
                     <div class="my-3 flex flex-col gap-2">
-                        <button class="py-2 px-6 bg-primary rounded-xl shadow-xl justify-center text-kuro font-inter text-sm font-semibold w-24 duration-75 ease-in-out hover:-translate-y-1" type="submit" name="delete">
+                        <button class="py-2 px-6 bg-primary rounded-xl shadow-xl justify-center text-kuro font-inter text-sm font-semibold w-24 duration-75 ease-in-out hover:-translate-y-1" type="submit" name="change">
                             UPDATE
                         </button>
-                        <p style="display: none; color: #FF2709;" id="cek">Please fill the form</p>
+                        <p style="display: none; color: #FF2709;" id="cek1">Please fill the form</p>
                         <p style="display: none; color: #FF2709;" id="cek3">User does not exist</p>
                     </div>
                     </form>
@@ -67,7 +67,7 @@ include("updateConf.php");
                     <p class="text-2xl font-inter font-semibold">Update Email: </p>
                 </div>
                 <div class="container">
-                    <form class="flex flex-col gap-2" action="updateConf.php"  method="POST">
+                    <form class="flex flex-col gap-2" action="updateEmailConf.php"  method="POST">
                     <div class="mb-3 flex flex-col gap-2">
                         <label class="text-lg font-inter font-medium">Old Email :</label>
                         <input class="border-2 border-[#9AA2B1] rounded-md w-64 py-1 px-3 placeholder:text-[#9AA2B1]" placeholder="Email yang ingin diubah..." type="text"  id="Email" name="email"  autocomplete="off" />
@@ -77,10 +77,10 @@ include("updateConf.php");
                         <input class="border-2 border-[#9AA2B1] rounded-md w-64 py-1 px-3 placeholder:text-[#9AA2B1]" placeholder="Email baru..." type="text"  id="Email" name="emailNew"  autocomplete="off" />
                     </div>
                     <div class="my-3 flex flex-col gap-2">
-                        <button class="py-2 px-6 bg-primary rounded-xl justify-center shadow-xl text-kuro font-inter text-sm font-semibold w-24 duration-75 ease-in-out hover:-translate-y-1" type="submit" name="delete">
+                        <button class="py-2 px-6 bg-primary rounded-xl justify-center shadow-xl text-kuro font-inter text-sm font-semibold w-24 duration-75 ease-in-out hover:-translate-y-1" type="submit" name="change">
                             UPDATE 
                         </button>
-                        <p style="display: none; color: #FF2709;" id="cek">Please fill the form</p>
+                        <p style="display: none; color: #FF2709;" id="cek2">Please fill the form</p>
                         <p style="display: none; color: #FF2709;" id="cek4">Email does not exist</p>
                     </div>
                     </form>
@@ -88,5 +88,28 @@ include("updateConf.php");
             </div>
         </div>
     </div>
+    <script>
+        let a = document.getElementById("cek1"); 
+        let b = document.getElementById("cek2"); 
+        let c = document.getElementById("cek3");
+        let d = document.getElementById("cek4");
+        if (window.location.href.indexOf("empty1") > -1) {
+            a.style.display = "block";
+        }
+        if (window.location.href.indexOf("empty2") > -1) {
+            b.style.display = "block";
+        }
+        if (window.location.href.indexOf("usernoexist") > -1) {
+            c.style.display = "block";
+        }
+        if (window.location.href.indexOf("emailnoexist") > -1) {
+            d.style.display = "block";
+        }
+        if (window.location.href.indexOf("empty") > -1) {
+            a.style.display = "block";
+            b.style.display = "block";
+        }
+
+    </script>
 </body>
 </html>
